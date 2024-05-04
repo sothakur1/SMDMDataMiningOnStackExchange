572f53da8010b595f9d1a727b0b57b7f00358cd5
Sohan Thakur <147930898+sothakur1@users.noreply.github.com>
2024-05-04 19:59:39 -0400

    ML model and analysis implemented
    Committed the code to implemented ML model and for further analysis

---
+++ technicalcorpus.py
@@ -0,0 +1,52 @@
+technical_terms = {
+    "javascript", "html", "css", "api", "http", "json",  # Web technologies
+    "python", "java","data","unstructured","semi-structured", "c++", "c#", "ruby", "perl", "php", "swift", "kotlin", "dart",  # Programming languages
+    "react", "angular", "vue", "django", "flask", "spring", "express", "laravel",  # Frameworks
+    "mysql", "postgresql", "mongodb", "sqlite", "redis", "oracle", "sql server", "svn", "mercurial", "github", "gitlab", "bitbucket",  # Version control systems
+    "docker", "kubernetes","variance","standard deviation","jenkins", "ansible", "express.js","mutual inclusion","correlation","terraform",  # DevOps tools
+    "rest","computer_science","graphql","cs","programming","scikit", "soap",  # API protocols
+    "linux", "windows", "macos", "ios", "android", "ubuntu", "centos", "debian", "fedora",  # Operating systems
+    "aws", "azure", "google cloud","linux","firebase", "heroku", "digitalocean", "linode",  # Cloud platforms
+    "html5", "css3", "ecmascript","hypothesis_testing","Roberta","hypothesis","data_science","data_mining","typescript", "xml", "yaml", "json",  # Data formats
+    "tensorflow", "aws","jdbc","pytorch", "keras", "scikit-learn", "pandas", "numpy", "matplotlib",  # Machine learning libraries
+    "typescript","snowflake","bigquery","linear regression","correlation","jinja","junit","pytest" ,"ruby","gradient","descent", "scala", "rust", "haskell","kmeans,","exploratory_data_analysis", "lua","cs", "clojure", "elixir", "groovy",
+    "perl","neural_network","lda","bert" ,"clustering","classification","bash", "powershell", "r", "matlab", "swift", "kotlin", "dart",
+    "xml", "yaml", "graphql","oops", "asp.net", "jsp", "node.js", "jquery", "bootstrap", "sass", "less", "tailwindcss",
+    "storybook", "webassembly", "three.js", "webgl", "socket.io", "grpc", "webpack", "babel",
+    "react", "angular", "string","vue", "django", "flask", "spring", "express", "laravel", "rails", "symfony", "meteor",
+    "next.js", "gatsby", "nuxt.js", "ionic", "cordova", "electron", "flutter", "qt", "wxwidgets",
+    "sqlite", "mongodb","adam","keras","ml","code_review","redis", "cassandra", "dynamodb", "neo4j", "couchbase", "firebase firestore", "realm",
+    "perforce", "dom","subversion", "pytorch","bitbucket","model","mvc","mining","controller","tree","tfs", "sourceforge", "bazaar",
+    "puppet", "chef", "mutex","git", "database", "saltstack", "vagrant", "gradle", "maven","flask",
+    "grpc", "thrift","cookie","Heteroscedasticity","protobuf","imputation","p-value", "confidence intervals", "R-squared" ,"json-rpc", "xml-rpc","algorithms","network","rpc",
+    "freebsd", "openbsd", "netbsd", "solaris", "interfaces", "exceptions", "iterators","aix", "hp-ux", "ios", "android", "unix",
+    "ibm cloud", "oracle cloud", "alibaba cloud", "salesforce cloud", "vmware cloud", "rackspace cloud",
+    "avro", "parquet", "protobuf", "orc", "msgpack",
+    "caffe", "mxnet", "theano", "caffe2", "torch", "h2o", "catboost", "xgboost", "lightgbm",
+    "vim", "emacs", "sublime text", "atom","list","eclipse", "intellij idea", "pycharm", "visual studio code",
+    "selenium", "appium", "junit", "testng", "mocha", "jest", "pytest", "cucumber", "robot framework",
+    "travis ci", "circleci", "teamcity", "buddy", "codeship", "cricleci", "spinnaker", "concourse",
+    "nagios", "zabbix", "prometheus", "grafana", "datadog", "new relic", "sentry", "appdynamics",
+    "rabbitmq", "kafka", "activemq", "mqtt", "nats", "zeromq", "redis pub/sub",
+    "nginx", "apache http server", "tomcat", "iis", "gunicorn", "uwsgi",
+    "podman", "rkt", "cri-o", "openvz", "lxd",
+    "vmware", "virtualbox", "hyper-v", "xen", "kvm", "qemu",
+    "soap", "xml-rpc", "atompub", "odata", "jsonapi", "graphql",
+    "wordpress", "drupal", "joomla", "magento", "shopify", "woocommerce", "wix", "squarespace",
+    "ethereum", "solidity", "hyperledger", "corda", "stellar", "ripple", "eos", "cardano",
+    "wireshark", "snort", "nmap", "metasploit", "owasp", "nessus", "acunetix", "burp suite",
+    "tcp/ip", "dns", "dhcp", "http/s", "ssl/tls", "ipv4", "ipv6", "icmp", "bgp", "ospf", "rip",
+    "hadoop", "spark", "hive", "pig", "hbase", "cassandra", "sqoop", "flume", "kafka", "storm",
+    "aws lambda", "google cloud functions", "azure functions", "ibm cloud functions", "openwhisk",
+    "talend", "pentaho", "matillion", "apach nifi", "informatica", "airflow", "luigi",
+    "mesos", "nomad", "swarmkit", "kontena", "rancher", "openstack", "nomad", "openshift",
+    "twitter api", "facebook api", "google maps api", "youtube api", "spotify api", "amazon s3 sdk", "google cloud storage sdk",
+    "unity", "unreal engine", "a-frame", "vuforia", "oculus", "htc vive", "magic leap", "hololens",
+    "arduino", "raspberry pi", "mqtt", "coap", "lorawan", "z-wave", "zigbee", "modbus",
+    "slack", "microsoft teams", "zoom", "webex", "googld meet", "trello", "asana", "jira", "basecamp",
+    "microsoft office", "google workspace", "notion", "evernote", "todoist", "ticktick", "asana", "toggl",
+    "agile", "scrum", "kanban", "lean", "waterfall", "devops", "ci/cd", "sdlc", "tdd", "bdd",
+    "pmi", "prince2", "pmi-pmbok", "ipma", "agilepm", "scrum.org","ann",
+    "ux design", "ui design", "usability testing", "wireframing", "prototyping", "sketch","chatgpt",
+    "arlene","cs_educators","artificial_intelligence","DenseNet","Resnet","List","queue","tree","cnns","robotics","backpropagation"
+}
